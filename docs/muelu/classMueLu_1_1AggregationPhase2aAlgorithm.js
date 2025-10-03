d040d9c9bfe42dc06d7a49d4342d0bb41d26f67f
cgcgcg <4666273+cgcgcg@users.noreply.github.com>
2025-10-03 18:10:12 +0000

    Update Doxygen

---
+++ docs/muelu/classMueLu_1_1AggregationPhase2aAlgorithm.js
@@ -0,0 +1,12 @@
+var classMueLu_1_1AggregationPhase2aAlgorithm =
+[
+    [ "AggregationPhase2aAlgorithm", "classMueLu_1_1AggregationPhase2aAlgorithm.html#ad567a68abcf75f69b5931d3d1f5a142c", null ],
+    [ "~AggregationPhase2aAlgorithm", "classMueLu_1_1AggregationPhase2aAlgorithm.html#a18b33e9fe8d8478f2ad98455c16bbbea", null ],
+    [ "SetupPhase", "classMueLu_1_1AggregationPhase2aAlgorithm.html#a84fc49e591221b3c1c149e61ca74b11f", null ],
+    [ "BuildAggregatesNonKokkos", "classMueLu_1_1AggregationPhase2aAlgorithm.html#a24bae9fae3b14d6d0489caeebeb74ea3", null ],
+    [ "BuildAggregates", "classMueLu_1_1AggregationPhase2aAlgorithm.html#aba59b0c0b46a3b30378dbaab27af4c11", null ],
+    [ "BuildAggregatesRandom", "classMueLu_1_1AggregationPhase2aAlgorithm.html#adcbe52824440b7a03bf607964e843ac4", null ],
+    [ "BuildAggregatesDeterministic", "classMueLu_1_1AggregationPhase2aAlgorithm.html#ab8189f5f9ec6b237d04e4c6f679b7004", null ],
+    [ "description", "classMueLu_1_1AggregationPhase2aAlgorithm.html#a824e6a6dc710cff1b8be20c0417295ea", null ],
+    [ "factorMLOverride_", "classMueLu_1_1AggregationPhase2aAlgorithm.html#a77d7e05e497b89dabf08180ac1b2aa25", null ]
+];
