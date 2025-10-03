2b2cae5dcded39d09109052b83af99d00224d0f2
cgcgcg <4666273+cgcgcg@users.noreply.github.com>
2025-10-03 21:07:16 +0000

    Update Doxygen

---
+++ docs/piro/search/classes_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['locaadaptivesolver_0',['locaadaptivesolver',['../classPiro_1_1Epetra_1_1LOCAAdaptiveSolver.html',1,'Piro::Epetra::LOCAAdaptiveSolver'],['../classPiro_1_1LOCAAdaptiveSolver.html',1,'Piro::LOCAAdaptiveSolver&lt; Scalar &gt;']]],
+  ['locasolver_1',['locasolver',['../classPiro_1_1Epetra_1_1LOCASolver.html',1,'Piro::Epetra::LOCASolver'],['../classPiro_1_1LOCASolver.html',1,'Piro::LOCASolver&lt; Scalar &gt;']]]
+];
