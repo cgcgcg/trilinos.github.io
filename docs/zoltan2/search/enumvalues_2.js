c7f3fe3c0b82f6213937b5829fdbc1b902036a4b
cgcgcg <4666273+cgcgcg@users.noreply.github.com>
2025-10-04 20:24:34 +0000

    Update Doxygen

---
+++ docs/zoltan2/search/enumvalues_2.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['cerr_5fstream_0',['CERR_STREAM',['../namespaceZoltan2.html#a842ab8d87036919da883b47a6963e3d7a0d4390935ae63379640678f3d208218d',1,'Zoltan2']]],
+  ['chaco_1',['CHACO',['../UserInputForTests_8hpp.html#ae1e69f3e78112064797477ddd0dbe1e9a7d27388cbbf135bc8f63efe1b7b5b374',1,'UserInputForTests.hpp']]],
+  ['circle_2',['CIRCLE',['../namespaceGeometricGen.html#a8fdada073d1b72c9b84e5abe9100e6b3ab842ee46abcc3c3bd46e34a82c4f3f00',1,'GeometricGen']]],
+  ['combinatorial_3',['COMBINATORIAL',['../classZoltan2_1_1Sphynx.html#a5279801bd5762fc4367459f7e3ed862aa0f701a86cfb345f86d80d8a353fa4bdc',1,'Zoltan2::Sphynx']]],
+  ['complex_5fassertion_4',['COMPLEX_ASSERTION',['../namespaceZoltan2.html#aa5647ff710d93088b7a1d04d16dbb28bafe551436a582fbd203ea658e7afdaf05',1,'Zoltan2']]],
+  ['coordinatemodeltype_5',['CoordinateModelType',['../namespaceZoltan2.html#a257358d4e8ba3fc91b1951b2eef936adadba4929e979c2c2496b25fcb63f23ac3',1,'Zoltan2']]],
+  ['cout_5fstream_6',['COUT_STREAM',['../namespaceZoltan2.html#a842ab8d87036919da883b47a6963e3d7aac5f587d80dfb173e64cb0415b4452e1',1,'Zoltan2']]],
+  ['cube_7',['CUBE',['../namespaceGeometricGen.html#a8fdada073d1b72c9b84e5abe9100e6b3ac08012971e35446c351270b92321f0b4',1,'GeometricGen']]]
+];
---
+++ docs/zoltan2/search/files_6.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['galeritest_2exml_0',['galeriTest.xml',['../galeriTest_8xml.html',1,'']]],
+  ['geometricgenerator_2ehpp_1',['GeometricGenerator.hpp',['../GeometricGenerator_8hpp.html',1,'']]],
+  ['geometrictest_2exml_2',['geometricTest.xml',['../geometricTest_8xml.html',1,'']]],
+  ['geometricvwgt2test_2exml_3',['geometricVwgt2Test.xml',['../geometricVwgt2Test_8xml.html',1,'']]],
+  ['geometricvwgttest_2exml_4',['geometricVwgtTest.xml',['../geometricVwgtTest_8xml.html',1,'']]],
+  ['glaze_2exml_5',['glaze.xml',['../glaze_8xml.html',1,'']]],
+  ['glaze2_2exml_6',['glaze2.xml',['../glaze2_8xml.html',1,'']]],
+  ['graph_2ecpp_7',['graph.cpp',['../graph_8cpp.html',1,'']]],
+  ['graphmodel_2ecpp_8',['GraphModel.cpp',['../GraphModel_8cpp.html',1,'']]],
+  ['graphmodel2ndadjsfromadjs_2ecpp_9',['GraphModel2ndAdjsFromAdjs.cpp',['../GraphModel2ndAdjsFromAdjs_8cpp.html',1,'']]]
+];
